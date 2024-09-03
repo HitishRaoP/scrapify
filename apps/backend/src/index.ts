8d9d1329e4ee385d1a870094fcbe2acea5c4908b
Hitish Rao P <hitishraop@gmail.com>
2024-09-03 14:27:53 +0530

    cors error try 10

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -2,21 +2,21 @@ import { config } from "dotenv";
 import cors from "cors";
 import express from "express";
 import { PORT } from "./constants";
-import { trpcExpress } from "@scrapify/trpc-server";
+import { trpcExpress } from "@scrapify/trpc-server/src/index";
 
 config();
 
 const app = express();
 
-app.use(cors())
+app.options("/trpc", cors())
 
 app.use(express.json());
 
 app.get("/", (req, res) => {
-    res.json({message : "server is up and running"});
+    res.json({ message: "server is up and running" });
 })
 
-app.use("/trpc", trpcExpress)
+app.use("/trpc", cors(), trpcExpress)
 
 app.listen(PORT, () => {
     console.log(`server running on http://localhost:${PORT}`);
--- packages/trpc-server/index.ts
+++
@@ -1,1 +0,0 @@
-export { trpcExpress } from "./src"
