88fc34dc6cfc2ed4eb6eda11317873160d703330
Hitish Rao P <hitishraop@gmail.com>
2024-09-04 20:23:50 +0530

    youtube tutorial

--- apps/backend/api/index.ts
+++ apps/backend/api/index.ts
@@ -1,24 +1,3 @@
-import { config } from "dotenv";
-import cors from "cors";
-import express from "express";
-import { PORT } from "../constants";
-import { trpcExpress } from "@scrapify/trpc-server";
-
-config()
-const app = express();
-
-app.options("/trpc", cors())
-
-app.use(express.json());
-
-app.get("/", (req, res) => {
-    res.json({ message: "server is up and running" });
-})
-
-app.use("/trpc", cors(), trpcExpress)
-
-app.listen(PORT, () => {
-    console.log("server running on http://localhost:${PORT}")
-})
+import app from "../src";
 
 export default app
